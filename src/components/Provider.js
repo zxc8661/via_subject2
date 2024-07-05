2622aad4ef60885506e7a60b00ec00b684a04cb3
zxc8661 <zxc866144@gmail.com>
2024-07-05 12:46:11 +0900

    V4

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,55 +1,8 @@
 import React, { useReducer, createContext, useRef, useContext } from 'react'
 
-const now = new Date()
-const formattedDate = now.toLocaleString('en-US', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false,
-})
+const posts = []
 
-const posts = [
-    {
-        id: 1,
-        title: 'a',
-        author: 'b',
-        date: formattedDate,
-        text: '2024.06.28. 15:41',
-        views: 0,
-        likes: 0,
-    },
-    {
-        id: 2,
-        title: 'e',
-        author: 'f',
-        date: formattedDate,
-        text: '2024.06.28. 15:41',
-        views: 0,
-        likes: 0,
-    },
-]
-
-const comments = [
-    {
-        id: 1,
-        postId: 1,
-        author: '베이비부엉이',
-        text: '올까요!',
-        date: '2024.06.28. 15:41',
-        likes: 0,
-    },
-    {
-        id: 2,
-        postId: 2,
-        author: '상땃쥐',
-        text: '온대여',
-        date: '2024.06.28. 15:41',
-        likes: 0,
-    },
-]
+const comments = []
 
 function Reducer(state, action) {
     switch (action.type) {
@@ -58,21 +11,6 @@ function Reducer(state, action) {
         case 'CORRECT':
             return state.map((post) =>
                 post.id === action.data.id ? action.data : post,
-            )
-        case 'DELETE':
-            return state.filter((data) => data.id !== action.id)
-        default:
-            return state
-    }
-}
-
-function commentReducer(state, action) {
-    switch (action.type) {
-        case 'WRITE':
-            return [...state, action.data]
-        case 'CORRECT':
-            return state.map((comment) =>
-                comment.id === action.data.id ? action.data : comment,
             )
         case 'DELETE':
             return state.filter((data) => data.id !== action.id)
@@ -91,7 +29,7 @@ const UserContext = createContext()
 export function Provider({ children }) {
     const [postsstate, Postdispatch] = useReducer(Reducer, posts)
     const [commentstate, Commentdispatch] = useReducer(Reducer, comments)
-    const nextId = useRef(3)
+    const nextId = useRef(1)
     const user = useRef('username')
 
     return (
