0c0864e499787b5a0940ea9a41502166442a9c0c
zxc8661 <zxc866144@gmail.com>
2024-07-09 14:21:12 +0900

    btest2

--- src/components/ToastEditor.js
+++ src/components/ToastEditor.js
@@ -1,43 +1,29 @@
-import React, { useEffect } from 'react'
-import { Editor } from '@toast-ui/react-editor'
-import '@toast-ui/editor/dist/toastui-editor.css'
+import React, { useEffect } from 'react';
+import { Editor } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
 
-const toolbar = [
-    ['heading', 'bold', 'italic', 'strike'],
-    ['hr', 'quote', 'ul', 'ol'],
-    ['image'],
-]
+type Props = {
+  editorRef: React.RefObject<Editor>;
+  content?: string;
+  setBody: (body: string) => void;
+};
 
-const TuiEditor = ({ content, editorRef, setBody }) => {
-    useEffect(() => {
-        if (content && editorRef.current) {
-            editorRef.current.getInstance().setHTML(content)
-        }
-    }, [content, editorRef])
+const toolbar = [['heading', 'bold', 'italic', 'strike'], ['hr', 'quote', 'ul', 'ol'], ['image']];
 
-    const onChangeGetHTML = () => {
-        if (editorRef.current) {
-            const data = editorRef.current.getInstance().getHTML()
-            setBody(data)
-        }
+const TuiEditor: React.FC<Props> = ({ content, editorRef, setBody }) => {
+  useEffect(() => {
+    if (content && editorRef.current) {
+      editorRef.current.getInstance().setHTML(content);
     }
+  }, [content, editorRef]);
 
-    return (
-        <Editor
-            initialValue={content ?? ' '}
-            initialEditType="wysiwyg"
-            autofocus={false}
-            ref={editorRef}
-            toolbarItems={toolbar}
-            hideModeSwitch={true}
-            height="500px"
-            usageStatistics={false}
-            onChange={onChangeGetHTML}
-        />
-    )
-}
+  const onChangeGetHTML = () => {
+    if (editorRef.current) {
+      const data = editorRef.current.getInstance().getHTML();
+      setBody(data); 
+    }
+  };
 
-<<<<<<< HEAD
   return (
     <Editor
       initialValue={content ?? ' '}
@@ -54,7 +40,6 @@ const TuiEditor = ({ content, editorRef, setBody }) => {
 };
 
 export default TuiEditor;
-=======
-export default TuiEditor
-//브랜치 연습중
->>>>>>> test1
+
+
+//브런치 태스트 2dddddd
